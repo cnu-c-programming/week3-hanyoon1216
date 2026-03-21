b7261a5ee4a703ce0cd76063682637bff004fb06
hanyoon1216 <hanyoon1216@gmail.com>
2026-03-21 17:14:25 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
+    int a = 10, b = 5;
+    printf("10 + 5 = %d\n", a + b);
+    printf("10 - 5 = %d\n", a - b);
+    printf("10 * 5 = %d\n", a * b);
+    printf("10 / 5 = %d\n", a / b);
 
     return 0;
 }
