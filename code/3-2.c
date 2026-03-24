30494dfb027c0927f693ba2bfe07b4f2808dfd77
hanyoon1216 <hanyoon1216@gmail.com>
2026-03-24 22:28:11 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -10,8 +10,8 @@ void test_scope() {
     static_var++;
     local_var++;
     printf("global_var: %d\n", global_var);
-    printf("global_var: %d\n", static_var);
-    printf("global_var: %d\n", local_var);
+    printf("static_var: %d\n", static_var);
+    printf("local_var: %d\n", local_var);
 }
 int main() {
 
